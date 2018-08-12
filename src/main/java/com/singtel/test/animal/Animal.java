641b61ab97cf6b9d5ff7335b4a872e0891b48ab1
nga251092 <nga251092@yahoo.com>
2018-08-12 17:44:03 +0800

    model a rooster without inheritance

--- src/main/java/com/singtel/test/animal/Animal.java
+++ src/main/java/com/singtel/test/animal/Animal.java
@@ -6,7 +6,7 @@ import static com.singtel.test.constant.Constants.DEFAULT_WALKING;
 
 public abstract class Animal {
     protected String sound;
-    protected Gender gender;
+    protected Gender gender = Gender.UNKNOWN;
 
     public void walk() {
         System.out.println(DEFAULT_WALKING);
